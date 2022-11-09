bf7e2fcd43965219178137695b5b21c8a3e7d7b4
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-11-09 19:52:50 +0530

    feat: added functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const thanks = document.querySelector(".thanks-container");
 const rating = document.getElementById("rating");
 const rates = document.querySelectorAll(".btn");
 const submitBtn = document.getElementById("submitBtn");
+const thanksMessage = document.getElementById("thanks-message");
 
 submitBtn.addEventListener("click", () => {
     thanks.classList.remove('hidden');
@@ -15,4 +16,8 @@ rates.forEach((rate) => {
     rate.addEventListener("click", () => {
         rating.innerHTML = rate.innerHTML;
     })
+    if (rating.innerHTML < 1) {
+        rating.innerHTML = 0;
+        thanksMessage.innerHTML = "You did'nt gave us any ratings."
+    }
 })
